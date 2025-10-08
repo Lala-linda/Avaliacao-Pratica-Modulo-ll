9d4946b3b5fa517f179ebb1928712fbee54152d0
lalais <laissanazarohdl@gmail.com>
2025-10-08 11:53:08 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,38 @@
-from flask import Flask,request, render_template
+from flask import Flask, request,jsonify, render_template
 
 app = Flask(__name__)
 
-numeros = [
-   {"valor1":5}
-   {"valor2":6}
-]
-
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/valores", methods=["GET"])
-def get_valores():
-    request.args.get("valor1")
+@app.route("/soma", methods = ["GET"])
+def soma():
+    valor1 = float(input(request.args.get("valor1",5)))
+    valor2 = float(input(request.args.get("valor1",6)))
+    resultado = valor1 + valor2
+    return jsonify({"continha": "soma", "resultado": resultado})
+
+@app.route("/subtração", methods = ["GET"])
+def subtração():
+    valor1 = float(input(request.args.get("valor1",5)))
+    valor2 = float(input(request.args.get("valor2",6)))
+    resultado = valor1 - valor2
+    return jsonify({"continha": "subtração", "resultado": resultado})
+
+@app.route("/multiplicação", methods = ["GET"])
+def multiplicação():
+    valor1 = float(input(request.args.get("valor1",5)))
+    valor2 = float(input(request.args.get("valor1",6)))
+    resultado = valor1 * valor2
+    return jsonify({"continha": "multiplicação", "resultado": resultado})
+
+@app.route("/divisão", methods = ["GET"])
+def divisão():
+    valor1 = float(input(request.args.get("valor1",5)))
+    valor2 = float(input(request.args.get("valor1",6)))
+    resultado = valor1 / valor2
+    return jsonify({"continha": "divisão", "resultado": resultado})
+
+if __name__ == "__main__":
+    app.run(debug=True)
